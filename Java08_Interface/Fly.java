bdb2ed59857e38b8c4352649b659f766b5daa7ba
Alexandre Gossard <119630621+FerniHop@users.noreply.github.com>
2023-10-02 21:09:58 +0200

    Add files via upload

---
+++ Java08_Interface/Fly.java
@@ -0,0 +1,20 @@
+public interface Fly {
+
+    void dive();
+
+    int upward(int meters);
+
+    int downward(int meters);
+
+    void getOut();
+
+    void takeOff();
+
+    int ascend(int meters);
+
+    void glide();
+
+    int descend(int meters);
+
+    void land();
+}
